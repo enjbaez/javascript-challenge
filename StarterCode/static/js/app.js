c3bcfe2e249d66ab6176c0a463a65ccca8108813
enjbaez <enjoli.baez@gmail.com>
2020-03-24 17:39:25 -0400

    javascript homework

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,42 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// reference the tbody
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+tbody.html("");
+data.forEach((dataRow )=>{
+    var Row = tbody.append('tr');
+    Object.values(dataRow).forEach((value)=>{
+        var cell = Row.append('td');
+        cell.text(value)
+    });
+});
+}
+
+buildTable(tableData)
+
+//Submit Button handler
+function handleclick() {
+    
+    // Select the date input value from the form
+    var date = d3.select("#datetime").property("value");
+    console.log(date);
+    
+    // Select the city input value from the form
+    var city = d3.select("#city").property("value");
+    console.log(city);   
+
+  
+    if (date !== "" && city !== "") {
+        // Filter the data if there was a date input
+        filteredData = tableData.filter(tableData => (tableData.datetime === date && tableData.city === city));
+    }
+    else {
+        filteredData = tableData;
+    };
+
+buildTable(filteredData);
+}
+d3.selectAll("#filter-btn").on("click", handleclick);
